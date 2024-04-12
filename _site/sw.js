d1555a174d380473c943313f09ef0337347baf23
Oliver Albertsen <oliveralbertsen@hotmail.com>
2024-04-12 16:54:43 +0200

    test3

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240412.163156';
+const cacheName = 'chirpy-20240412.165356';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
